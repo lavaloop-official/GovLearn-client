97a9ccccc6ea0a85666072a188f2095fac421e90
Fabian <1803fabian@gmail.com>
2023-10-24 15:39:37 +0200

    incorperate new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import './App.css'
-import {Avatar, Carousel, ConfigProvider, Layout} from "antd";
-import {UserOutlined} from '@ant-design/icons';
+import {Carousel, ConfigProvider, Divider, Layout} from "antd";
 import {Content, Header} from "antd/es/layout/layout";
-import Search from "antd/es/input/Search";
-import {Typography} from 'antd';
 import CarouselPane from './CarouselPane.tsx';
+import CustomHeader from "./CustomHeader.tsx";
+import RecomSlider from "./RecomSlider.tsx";
 
-const {Title} = Typography
 
 function App() {
     return (
@@ -28,41 +26,54 @@ function App() {
                     zIndex: "10000",
                     position: "sticky",
                     top: "0",
-                    height: "76px"
+                    height: "100%"
                 }}>
-                    <div style={{width: "100%", height: "56px"}}>
-                        <div style={{
-                            margin: "0 auto",
-                            maxWidth: "1200px",
-                            width: "100%",
-                            height: "100%",
-                            display: "flex",
-                            verticalAlign: "middle",
-                            gap: "10px",
-                            padding: "0px 10px",
-                        }}>
-                            <Title level={3} style={{margin: "auto auto auto 0px", minWidth: "100px"}}>Govlearn</Title>
-                            <Search placeholder="Kursangebote suchen" style={{maxWidth: "400px", margin: "auto"}}/>
-                            <Avatar icon={<UserOutlined/>} style={{margin: "auto 0px auto auto", minWidth: "32px"}}/>
-                        </div>
-                        <div style={{width: "100%", height: "20px", background: "red"}}>
-                        </div>
-                    </div>
-
+                    <CustomHeader/>
                 </Header>
-                <Content style={{zIndex: "1", display: "flex"}}>
+                <Content style={{
+                    zIndex: "1",
+                    display: "flex",
+                    justifyContent: "center",
+                    flexDirection: "column",
+                    gap: "10px"
+                }}>
                     <div style={{maxWidth: "1200px", margin: "auto", width: "100%", padding: "10px 10px"}}>
                         <Carousel autoplay={true} effect="fade">
                             <div>
-                                <CarouselPane text="oi9sugsiouebgiu"/>
+                                <CarouselPane text="Bilden sie sich weiter jetzt hallo"/>
                             </div>
                             <div>
-                                <CarouselPane text="lalalalalalal das ist ein Weiterbildungsangebot" src="https://www.langweiledich.net/wp-content/uploads/2018/03/die-skurrilsten-wtf-stock-photos_01.jpg"/>
+                                <CarouselPane text="lalalalalalal das ist ein Weiterbildungsangebot"
+                                              src="https://www.langweiledich.net/wp-content/uploads/2018/03/die-skurrilsten-wtf-stock-photos_01.jpg"/>
                             </div>
                             <div>
                                 <CarouselPane text="2"/>
                             </div>
+                            <div>
+                                <CarouselPane/>
+                            </div>
                         </Carousel>
+                    </div>
+                    <div style={{
+                        zIndex: "1",
+                        display: "flex",
+                        justifyContent: "center",
+                        flexDirection: "column",
+                        gap: "10px",
+                        maxWidth: "1200px",
+                        width: "100%",
+                        margin: "auto",
+                    }}>
+                        <Divider style={{margin: "0px", maxWidth: "1100px"}}/>
+                        <RecomSlider/>
+                        <Divider style={{margin: "0px"}}/>
+                        <RecomSlider/>
+                        <Divider style={{margin: "0px"}}/>
+                        <RecomSlider/>
+                        <Divider style={{margin: "0px"}}/>
+                        <RecomSlider/>
+                        <Divider style={{margin: "0px"}}/>
+                        <RecomSlider/>
                     </div>
                 </Content>
             </Layout>
