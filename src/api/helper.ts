fd2ae1f1e1a9a982d5f5be14c223a8b5876a7fcb
Fabian <1803fabian@gmail.com>
2023-11-14 15:49:12 +0100

    refactored fetchwrapper + added documentation

--- src/api/helper.ts
+++ src/api/helper.ts
@@ -1,6 +1,15 @@
 import {BACKEND_URL} from "../constants/backend";
 import reduxStore from "../state/reduxStore";
 
+//TODO: add proper error handling
+
+/**
+ * Wrapper for fetch that automatically adds the auth token to the header
+ * usage: fetchWrapper.get("/api/endpoint") or fetchWrapper.post("/api/endpoint", body) ... etc
+ * @param url api endpoint to call (backend url is automatically prepended)
+ * @param body optional body for POST and PUT requests
+ * @returns response body
+ */
 export const fetchWrapper = {
     get: request('GET'),
     post: request('POST'),
@@ -8,54 +17,56 @@ export const fetchWrapper = {
     delete: request('DELETE')
 };
 
-function request(method) {
-    return (url, body) => {
+function request(method: string) {
+    return async (url: string, body?: never) => {
         const fullurl = BACKEND_URL + url;
-        const requestOptions = {
-            method,
-            headers: authHeader(fullurl),
-        };
-        if (body) {
-            requestOptions.headers['Content-Type'] = 'application/json';
-            requestOptions.body = JSON.stringify(body);
-        }
-        return fetch(fullurl, requestOptions).then(handleResponse);
+
+        const response = await fetch(fullurl, {
+                method,
+                headers: generateHeader(fullurl, method),
+                body: body ? JSON.stringify(body) : null
+            }
+        ).catch(
+            (error) => {
+                console.log(error)
+                return handleError(error)
+            }
+        );
+
+        return handleResponse(response);
     }
 }
 
-function authHeader(url) {
-    // return auth header with jwt if user is logged in and request is to the api url
-    const token = authToken();
-    const isLoggedIn = !!token;
-    const isApiUrl = url.startsWith(BACKEND_URL);
-    if (isLoggedIn && isApiUrl) {
-        return { Authorization: `Bearer ${token}` };
-    } else {
-        return {};
-    }
+function generateHeader(url: string, method: string) {
+    const header: HeadersInit = new Headers()
+    if (authToken() && url.startsWith(BACKEND_URL))
+        header.set("Authorization", "Bearer " + authToken())
+    if (method !== "GET")
+        header.set("Content-Type", "application/json")
+
+    return header
 }
 
 function authToken() {
     return reduxStore.getState().auth.authtoken;
 }
 
-function handleResponse(response) {
+async function handleResponse(response: Response | undefined) {
+    if (!response)
+        return handleError(new Error("No response"))
+    
     return response.text().then(text => {
         const data = text && JSON.parse(text);
 
-        /*
         if (!response.ok) {
-            if ([401, 403].includes(response.status) && authToken()) {
-                // auto logout if 401 Unauthorized or 403 Forbidden response returned from api
-                const logout = () => store.dispatch(authActions.logout());
-                logout();
-            }
-
-            const error = (data && data.message) || response.statusText;
-            return Promise.reject(error);
+            return undefined;
         }
-        */
 
         return data;
     });
+}
+
+function handleError(error: any) {
+    console.log(error)
+    return undefined;
 }
