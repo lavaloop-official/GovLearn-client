e8c40d3b4cbf4c2e987658cfed2184a2e9706991
Julian Sibbing <jsibbing@uni-muenster.de>
2023-11-15 20:15:36 +0100

    replaced custom buton and formatting

---
+++ src/components/AnimatedButton.tsx
@@ -0,0 +1,26 @@
+import React, { ReactNode, useState } from "react";
+import "./Button.css";
+
+interface MyButtonComponentProps {
+  children: ReactNode;
+  onClick: any;
+}
+
+function AnimatedButton({ children, onClick }: MyButtonComponentProps) {
+  const [isHovered, setIsHovered] = useState(false);
+
+  return (
+    <button
+      id="animatedButton"
+      onClick={onClick}
+      className={`animated-button ${isHovered ? "hovered" : ""}`}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      style={{ background: "cornflowerblue" }}
+    >
+      {children}
+    </button>
+  );
+}
+
+export default AnimatedButton;
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,117 +1,171 @@
-import './Landing.css';
-import {Button, Card, Flex, Typography} from 'antd';
-import {ReadOutlined, UserOutlined, PartitionOutlined, DoubleRightOutlined} from '@ant-design/icons';
-import {openLoginModal} from "../state/modalutil.ts";
+import "./Landing.css";
+import { Button, Card, Flex, Typography } from "antd";
+import {
+  ReadOutlined,
+  UserOutlined,
+  PartitionOutlined,
+  ArrowRightOutlined,
+} from "@ant-design/icons";
+import { openLoginModal } from "../state/modalutil.ts";
 import LoginModal from "../components/Login/LoginModal.tsx";
+import { useState } from "react";
+import AnimatedButton from "../components/AnimatedButton.tsx";
 
 function Landing() {
+  const cardStyle: React.CSSProperties = {
+    width: 1360,
+  };
 
-    const cardStyle: React.CSSProperties = {
-        width: 1440,
-    };
+  const attributeCardStyle: React.CSSProperties = {
+    width: 256,
+    height: 200,
+    backgroundColor: "cornflowerblue",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  };
 
-    const attributeCardStyle: React.CSSProperties = {
-        width: 256,
-        height: 200,
-        backgroundColor: 'cornflowerblue',
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
+  const imgStyle: React.CSSProperties = {
+    display: "block",
+    width: 756,
+  };
 
-    };
+  const complementColor: React.CSSProperties = {
+    backgroundColor: "cornflowerblue",
+  };
 
-    const imgStyle: React.CSSProperties = {
-        display: 'block',
-        width: 756,
-    };
+  const flexStyle: React.CSSProperties = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
+  };
 
-    const complementColor: React.CSSProperties = {
-        backgroundColor: 'cornflowerblue'
-    }
+  const flexCardStyle = { ...cardStyle, ...flexStyle };
 
-    const flexStyle: React.CSSProperties = {
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexDirection: 'row'
-    };
-
-    const flexCardStyle = {...cardStyle, ...flexStyle};
-
-    return (
-        <div style={{
-            zIndex: "1",
-            display: "flex",
-            justifyContent: "center",
-            flexDirection: "row",
-            gap: "10px"
-        }}>
-            <Flex vertical justify='center'>
-                <Card style={cardStyle} bodyStyle={{padding: 0, overflow: 'hidden'}}>
-                    <Flex justify="space-between">
-                        <img
-                            alt="avatar"
-                            src="https://images.pexels.com/photos/4065891/pexels-photo-4065891.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                            style={imgStyle}
-                        />
-                        <Flex vertical align="flex-end" justify="space-between" style={{padding: 64}}>
-                            <Typography.Title level={2}>
-                                GovLearn - Deine Weiterbildungsangebotsplattform
-                            </Typography.Title>
-                            <Typography.Title level={3}>
-                                Finde Weiterbildungsangebote die dir weiterhelfen.
-                            </Typography.Title>
-                        </Flex>
-                    </Flex>
-                </Card>
-
-                <Card style={cardStyle} bodyStyle={complementColor}>
-                    <div className="flip-container">
-                        <Flex justify='space-around'>
-                            <div className="flip-box">
-                                <div className="flip-box-inner">
-                                    <div className="flip-box-front"><Card style={attributeCardStyle}><ReadOutlined
-                                        style={{fontSize: '64px', color: 'white'}}/><p
-                                        style={{color: 'white', fontSize: 'large'}}>großes Angebot an
-                                        Weiterbildungsangeboten</p></Card></div>
-                                    <div className="flip-box-back"><Card style={attributeCardStyle}><p
-                                        style={{color: 'white', fontSize: 'large'}}>BlalblvBlalblvapsfanglibahdbgah b bq
-                                        q qbnfiwqlb fqasfnafafasfapsfanglibahdbgah b bq q qbnfiwqlb fqasfnafafasf</p>
-                                    </Card></div>
-                                </div>
-                            </div>
-                            <div className="flip-box">
-                                <div className="flip-box-inner">
-                                    <div className="flip-box-front"><Card style={attributeCardStyle}><UserOutlined
-                                        style={{fontSize: '64px', color: 'white'}}/><p
-                                        style={{color: 'white', fontSize: 'large'}}>Finde das Angebot das für dich am
-                                        besten passt</p></Card></div>
-                                    <div className="flip-box-back"><Card style={attributeCardStyle}><p
-                                        style={{color: 'white', fontSize: 'large'}}>Unsere über jahrzehnte entwickelte
-                                        Algorithmik erlaubt es uns mittel künstlicher Intelligenz, Weiterbildungsangebot
-                                        vorzuschlagen, welche ihre Bedürfnisse erfüllen.</p></Card></div>
-                                </div>
-                            </div>
-                            <div className="flip-box">
-                                <div className="flip-box-inner">
-                                    <div className="flip-box-front"><Card style={attributeCardStyle}><PartitionOutlined
-                                        style={{fontSize: '64px', color: 'white'}}/><p
-                                        style={{color: 'white', fontSize: 'large'}}>Ordne deinem Team Angebote zu</p>
-                                    </Card></div>
-                                    <div className="flip-box-back"><Card style={attributeCardStyle}><p
-                                        style={{color: 'white', fontSize: 'large'}}>Blalblvapsfanglibahdbgah b bq q
-                                        qbnfiwqlb Blalblvapsfanglibahdbgah b bq q qbnfiwqlb
-                                        fqasfnafafasffqasfnafafasf</p></Card></div>
-                                </div>
-                            </div>
-                        </Flex>
-                    </div>
-                </Card>
+  return (
+    <div
+      style={{
+        zIndex: "1",
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: "row",
+        gap: "10px",
+      }}
+    >
+      <Flex vertical justify="center">
+        <Card style={cardStyle} bodyStyle={{ padding: 0, overflow: "hidden" }}>
+          <Flex justify="space-between">
+            <img
+              alt="avatar"
+              src="https://images.pexels.com/photos/4065891/pexels-photo-4065891.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              style={imgStyle}
+            />
+            <Flex
+              vertical
+              align="flex-start"
+              justify="space-between"
+              style={{ padding: 64 }}
+            >
+              <Typography.Title level={2}>
+                GovLearn - Deine Weiterbildungsangebotsplattform für den
+                öffentlichen Dienst
+              </Typography.Title>
+              <Typography.Title level={3}>
+                Entdecke Weiterbildungsangebote, die den öffentlichen Dienst
+                voranbringen.
+              </Typography.Title>
+              <AnimatedButton
+                onClick={() => {
+                  openLoginModal("register");
+                }}
+              >
+                Jetzt Loslegen!
+              </AnimatedButton>
             </Flex>
-            <LoginModal/>
-        </div>
-    )
+          </Flex>
+        </Card>
+
+        <Card style={cardStyle} bodyStyle={complementColor}>
+          <div className="flip-container">
+            <Flex justify="space-around">
+              <div className="flip-box">
+                <div className="flip-box-inner">
+                  <div className="flip-box-front">
+                    <Card style={attributeCardStyle}>
+                      <ReadOutlined
+                        style={{ fontSize: "64px", color: "white" }}
+                      />
+                      <p style={{ color: "white", fontSize: "large" }}>
+                        großes Angebot an Weiterbildungsangeboten
+                      </p>
+                    </Card>
+                  </div>
+                  <div className="flip-box-back">
+                    <Card style={attributeCardStyle}>
+                      <p style={{ color: "white", fontSize: "large" }}>
+                        Unser breites Spektrum an Weiterbildungsangeboten
+                        ermöglicht es dir, deine Fähigkeiten zu erweitern und
+                        beruflich voranzukommen.
+                      </p>
+                    </Card>
+                  </div>
+                </div>
+              </div>
+              <div className="flip-box">
+                <div className="flip-box-inner">
+                  <div className="flip-box-front">
+                    <Card style={attributeCardStyle}>
+                      <UserOutlined
+                        style={{ fontSize: "64px", color: "white" }}
+                      />
+                      <p style={{ color: "white", fontSize: "large" }}>
+                        Finde das Angebot das für dich am besten passt
+                      </p>
+                    </Card>
+                  </div>
+                  <div className="flip-box-back">
+                    <Card style={attributeCardStyle}>
+                      <p style={{ color: "white", fontSize: "large" }}>
+                        Unsere Plattform nutzt moderne Technologie, um dir
+                        gezielt Weiterbildungsangebote vorzuschlagen, die deinen
+                        Bedürfnissen entsprechen.
+                      </p>
+                    </Card>
+                  </div>
+                </div>
+              </div>
+              <div className="flip-box">
+                <div className="flip-box-inner">
+                  <div className="flip-box-front">
+                    <Card style={attributeCardStyle}>
+                      <PartitionOutlined
+                        style={{ fontSize: "64px", color: "white" }}
+                      />
+                      <p style={{ color: "white", fontSize: "large" }}>
+                        Ordne deinem Team Angebote zu
+                      </p>
+                    </Card>
+                  </div>
+                  <div className="flip-box-back">
+                    <Card style={attributeCardStyle}>
+                      <p style={{ color: "white", fontSize: "large" }}>
+                        Unsere Plattform ermöglicht es dir,
+                        Weiterbildungsangebote effizient an dein Team zuzuweisen
+                        und deren berufliche Entwicklung zu fördern.
+                      </p>
+                    </Card>
+                  </div>
+                </div>
+              </div>
+            </Flex>
+          </div>
+        </Card>
+      </Flex>
+      <LoginModal />
+    </div>
+  );
 }
 
-export default Landing
+export default Landing;
