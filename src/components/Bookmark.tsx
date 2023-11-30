b1eada54b84275ba0dc66fe35eda1a5859ab016c
Fabian <1803fabian@gmail.com>
2023-11-30 20:54:58 +0100

    made bookmark more independent of other components and dependent of global state

--- src/components/Bookmark.tsx
+++ src/components/Bookmark.tsx
@@ -1,15 +1,24 @@
 import {StarFilled, StarOutlined} from "@ant-design/icons";
 import "./Bookmark.css";
+import {useDispatch, useSelector} from "react-redux";
+import {RootState} from "../state/reduxStore.ts";
+import {changeBookmark} from "../state/bookmarkslice.ts";
+import React from "react";
+import {Button} from "antd";
 
-function Bookmark({bookmark, click}: { bookmark?: boolean, click?: () => void }) {
+function Bookmark({id, style}: { id: number, style?: React.CSSProperties }) {
+
+    const marked = useSelector((state: RootState) => state.bookmark.bookmarks.includes(id))
+    const dispatch = useDispatch()
+    const onclick = () => {
+        dispatch(changeBookmark(id))
+    }
 
     return (
-        <div id="bookmark_outer" onClick={click}>
-            {
-                bookmark ? <StarFilled id="bookmark_inner"/> :
-                    <StarOutlined id="bookmark_inner"/>
-            }
-        </div>
+        <Button id="bookmark_outer" type="text" onClick={onclick} shape="circle" style={style} icon={
+            marked ? <StarFilled id="bookmark_inner"/> : <StarOutlined id="bookmark_inner"/>
+        }>
+        </Button>
     );
 }
 
