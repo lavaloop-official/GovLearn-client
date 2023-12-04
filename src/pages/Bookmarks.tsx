220f743792f5d25bc933939ade63c6d31420795d
Fabian <1803fabian@gmail.com>
2023-12-04 20:24:57 +0100

    first draft of bookmarks page

--- src/pages/Bookmarks.tsx
+++ src/pages/Bookmarks.tsx
@@ -1,15 +1,22 @@
-import {useSelector} from "react-redux";
-import {RootState} from "../state/reduxStore.ts";
+import {useEffect, useState} from "react";
+import {Course} from "../interfaces.ts";
+import {fetchWrapper} from "../api/helper.ts";
 
 function Bookmarks() {
 
-    const bookmarks = useSelector((state: RootState) => state.bookmark.bookmarks)
+    const [bookmarks, setBookmarks] = useState<Course[]>([])
+
+    useEffect(() => {
+        fetchWrapper.get("api/v1/bookmarks").then((res) => {
+            setBookmarks(res.payload)
+        })
+    }, []);
 
     return (
         <div>
             <h1>Bookmarks</h1>
             <ul>
-                {bookmarks.map((id) => <li>{id}</li>)}
+                {bookmarks.map((course) => <li key={course.id}>{course.name}</li>)}
             </ul>
         </div>
     );
