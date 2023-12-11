ca0db3e957efa048ec1a7c49ed12f26c4fb49379
Julian Sibbing <jsibbing@uni-muenster.de>
2023-12-11 21:45:10 +0100

    fixed attributes

--- src/pages/Details.tsx
+++ src/pages/Details.tsx
@@ -1,11 +1,11 @@
-import {Affix, Button, Card, Flex, Image, Rate, Skeleton} from "antd";
-import {useEffect, useState} from "react";
-import {ArrowLeftShort} from "react-bootstrap-icons";
+import { Affix, Button, Card, Flex, Image, Rate, Skeleton } from "antd";
+import { useEffect, useState } from "react";
+import { ArrowLeftShort } from "react-bootstrap-icons";
 import Recommendation from "../components/Recommendation.tsx";
 import Feedback from "../components/Detail/Feedback.tsx";
-import {fetchWrapper} from "../api/helper.ts";
+import { fetchWrapper } from "../api/helper.ts";
 import './Details.css';
-import {Course, Review} from "../interfaces.ts";
+import { Course, Review } from "../interfaces.ts";
 import ReviewComp from "../components/Detail/ReviewComp.tsx";
 import Bookmark from "../components/Bookmark.tsx";
 
@@ -73,7 +73,7 @@ function Details() {
                 style={{
                     justifyContent: "center"
                 }}>
-                <div style={{maxWidth: "1200px", width: "100%"}}>
+                <div style={{ maxWidth: "1200px", width: "100%" }}>
                     <Flex
                         gap="small"
                         style={{
@@ -92,18 +92,18 @@ function Details() {
                                 lineHeight: "0px",
                                 alignSelf: "center",
                             }}
-                            icon={<ArrowLeftShort size={40}/>}
+                            icon={<ArrowLeftShort size={40} />}
                         />
-                        <h1 style={{margin: "0", padding: "0 15px"}}>{course.name}</h1>
+                        <h1 style={{ margin: "0", padding: "0 15px" }}>{course.name}</h1>
                     </Flex>
                     <Flex>
                         <Flex vertical className="course-main"
-                              style={{
-                                  maxWidth: "1200px",
-                                  width: "100%",
-                                  margin: "auto",
-                                  padding: "10px",
-                              }}
+                            style={{
+                                maxWidth: "1200px",
+                                width: "100%",
+                                margin: "auto",
+                                padding: "10px",
+                            }}
                         >
                             <div
                                 style={{
@@ -144,22 +144,22 @@ function Details() {
                                                 borderRadius: "15px",
                                                 width: "100%",
                                                 height: "100%",
-                                            }}/>
+                                            }} />
                                         </div>
                                     )
                                 }
 
 
                                 <Flex className="course-sidebar" vertical gap="middle"
-                                      style={{
-                                          maxWidth: "200px",
-                                          width: "100%",
-                                          padding: "5px",
-                                      }}
+                                    style={{
+                                        maxWidth: "200px",
+                                        width: "100%",
+                                        padding: "5px",
+                                    }}
                                 >
-                                    <Card className="antcard" style={{height: "100%"}}>
+                                    <Card className="antcard" style={{ height: "100%" }}>
                                         {course.id ? <Bookmark id={course.id}
-                                                               style={{position: "absolute", top: "10px", right: "10px"}}/> : <></>}
+                                            style={{ position: "absolute", top: "10px", right: "10px" }} /> : <></>}
                                         <div className="course-details" style={{
                                             padding: "0px",
                                             maxWidth: "190px",
@@ -189,16 +189,27 @@ function Details() {
                                                     <p className="attribute-value">{new Date(course.startDate).toLocaleDateString('DE')}</p>
                                                 </div>
                                             )}
+                                            {course.certificate && (
+                                                <div className="course-attribute">
+                                                    <p className="attribute-label">Zertifizierung:</p>
+                                                    <p className="attribute-value">{course.costFree ? 'vorhanden' : 'nicht vorhanden'}</p>
+                                                </div>
+                                            )}
+                                            {course.costFree && (
+                                                <div className="course-attribute">
+                                                    <p className="attribute-label">Kosten:</p>
+                                                    <p className="attribute-value">{course.costFree ? 'kostenlos' : 'kostenpflichtig'}</p>
+                                                </div>
+                                            )}
                                             {course.domainSpecific && (
                                                 <div className="course-attribute">
-                                                    <p className="attribute-label">Verwaltungsbezogen:</p>
-                                                    <p className="attribute-value">{course.domainSpecific ? "Ja" : "Nein"}</p>
+                                                    <p className="attribute-label">Verwaltungsbezogen</p>
                                                 </div>
                                             )}
                                         </div>
                                     </Card>
-                                    <Button style={{margin: "5px", borderRadius: "15px"}} type="primary" size="large"
-                                            href={course.link ? course.link : undefined}>
+                                    <Button style={{ margin: "5px", borderRadius: "15px" }} type="primary" size="large"
+                                        href={course.link ? course.link : undefined}>
                                         Zum Angebot
                                     </Button>
                                 </Flex>
@@ -212,19 +223,19 @@ function Details() {
                                     marginTop: "5px"
                                 }}
                             >
-                                <Flex style={{justifyContent: "space-between", width: "100%"}}>
-                                    <Card className="antcard" style={{margin: "5px", width: "70%"}}>
-                                        <p style={{fontWeight: "bold"}}>Beschreibung</p>
+                                <Flex style={{ justifyContent: "space-between", width: "100%" }}>
+                                    <Card className="antcard" style={{ margin: "5px", width: "70%" }}>
+                                        <p style={{ fontWeight: "bold" }}>Beschreibung</p>
                                         {course.description ? (
                                             <p>{course.description}</p>
                                         ) : (
                                             <p>keine Beschreibung vorhanden.</p>
                                         )}
                                     </Card>
-                                    <Card className="antcard" style={{margin: "5px", width: "30%"}}>
+                                    <Card className="antcard" style={{ margin: "5px", width: "30%" }}>
                                         <Flex justify="space-evenly">
                                             <Image
-                                                style={{borderRadius: '50%', width: '100px', height: '100px'}}
+                                                style={{ borderRadius: '50%', width: '100px', height: '100px' }}
                                                 // TODO: Bilder von Instructor einfügen
                                                 src="https://img.myloview.de/sticker/default-profile-picture-avatar-photo-placeholder-vector-illustration-700-205664584.jpg"
                                                 alt="Bild konnte nicht geladen werden"
@@ -253,10 +264,10 @@ function Details() {
                                     marginBottom: "5px"
                                 }}
                             >
-                                <Flex style={{justifyContent: "space-between", width: "100%"}}>
-                                    <Card className="antcard" style={{margin: "5px", width: "30%"}}>
-                                        <p style={{fontWeight: "bold"}}>Durchschnittsbewertung</p>
-                                        <Rate disabled value={averageRating}/>
+                                <Flex style={{ justifyContent: "space-between", width: "100%" }}>
+                                    <Card className="antcard" style={{ margin: "5px", width: "30%" }}>
+                                        <p style={{ fontWeight: "bold" }}>Durchschnittsbewertung</p>
+                                        <Rate disabled value={averageRating} />
                                     </Card>
                                     <ReviewComp id={course.id}></ReviewComp>
                                 </Flex>
@@ -269,20 +280,20 @@ function Details() {
                                 }}
                             >
                                 <Flex vertical gap="small" className="course-feedback"
-                                      style={{margin: "5px", borderRadius: "15px"}}>
+                                    style={{ margin: "5px", borderRadius: "15px" }}>
                                     {feedback.length > 0 ? (
                                         feedback.map((feedbackItem) => (
                                             <Feedback review={feedbackItem} key={feedbackItem.feedbackID}></Feedback>
                                         ))
                                     ) : (
-                                        <Card className="antcard" style={{justifyContent: "center"}}>keine Bewertungen
+                                        <Card className="antcard" style={{ justifyContent: "center" }}>keine Bewertungen
                                             vorhanden.</Card>
                                     )}
                                 </Flex>
                             </div>
                         </Flex>
                         <Affix offsetTop={90}>
-                            <div style={{width: "100%", padding: "10px", display: "flex"}}>
+                            <div style={{ width: "100%", padding: "10px", display: "flex" }}>
                                 <div
                                     style={{
                                         background: "#d9d9d9",
@@ -305,12 +316,12 @@ function Details() {
                                     </h2>
                                     {relatedCourses.length > 0 ?
                                         relatedCourses.map((course) => (
-                                            <Recommendation obj={course} key={course.id}/>
+                                            <Recommendation obj={course} key={course.id} />
                                         )) :
                                         <>
-                                            <Recommendation/>
-                                            <Recommendation/>
-                                            <Recommendation/>
+                                            <Recommendation />
+                                            <Recommendation />
+                                            <Recommendation />
                                         </>
                                     }
 
