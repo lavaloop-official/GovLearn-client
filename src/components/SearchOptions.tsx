2e9f892bbcb68118f73ae882dd707ce2d48c370d
Fabian <1803fabian@gmail.com>
2023-12-13 20:37:38 +0100

    search improvements

--- src/components/SearchOptions.tsx
+++ src/components/SearchOptions.tsx
@@ -1,188 +1,24 @@
-import {Card, Checkbox, DatePicker, DatePickerProps, Divider, Select, Slider, Space} from 'antd';
-import type { CheckboxValueType } from 'antd/es/checkbox/Group';
-import type { SelectProps } from 'antd';
-import { SliderMarks, SliderTooltipProps } from "antd/es/slider/index";
-import { CourseFilterWsTo} from '../interfaces';
-import {Format, Skilllevel} from '../Enum';
-import { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
-import { fetchWrapper } from '../api/helper';
+import {Card, Checkbox, DatePicker, DatePickerProps, Divider, Select, Slider} from 'antd';
+import type {CheckboxValueType} from 'antd/es/checkbox/Group';
+import type {SelectProps} from 'antd';
+import {SliderMarks, SliderTooltipProps} from "antd/es/slider/index";
+import {CourseFilterWsTo} from '../interfaces';
+import {useEffect, useState} from 'react';
+import {fetchWrapper} from '../api/helper';
+import './SearchOptions.css';
 
-interface SearchOptionsProps {
-    
-    onVariableChange: (variable: CourseFilterWsTo) => void;
-  }
-
-const SearchOptions: React.FC<SearchOptionsProps> = ({onVariableChange}) => {
-
-    const loc = useLocation();
-
-    const buildCourseFilterWsTo = () => {
-        let courseFilter: CourseFilterWsTo = {
-            tagIDs: loc.state?.tagIDs,
-            Anbieter: Anbieter,
-            Wissensbezug: Wissensbezug,
-            Verwaltungsspezifisch: Verwaltungsspezifisch,
-            Zertifikat: Zertifikat,
-            Kompetenzstufe: Kompetenzstufen,
-            Format: Formate,
-            Startdatum: Startdatum,
-            Dauer: Dauer,
-            Kosten: Kosten,
-            Sonstiges: []
-        }
-        onVariableChange(courseFilter);
-    }
-
-    const [Anbieter, setAnbieter] = useState<string[]>([]);
-    const [Wissensbezug, setWissensbezug] = useState<string[]>();
-    const [Verwaltungsspezifisch, setVerwaltungsspezifisch] = useState<boolean>();
-    const [Zertifikat, setZertifikat] = useState<boolean>();
-    const [Kompetenzstufen, setKompetenzstufen] = useState<Skilllevel[]>([Skilllevel.Anfaenger, Skilllevel.Fortgeschritten, Skilllevel.Experte]);
-    const [Formate, setFormat] = useState<Format[]>([Format.OnlineLive, Format.Praesenz, Format.Hybrid, Format.OnlineSelbstorganisiert]);
-    const [Startdatum, setStartdatum] = useState<Date>();
-    const [Dauer, setDauer] = useState<string[]>();
-    const [Kosten, setKosten] = useState<boolean>(false);
-    const [Sonstiges, setSonstiges] = useState<string[]>();
+function SearchOptions({courseFilter, onFilterChange}: {
+    courseFilter: CourseFilterWsTo,
+    onFilterChange: (variable: object) => void
+}) {
 
     const [options, setOptions] = useState<SelectProps['options']>([]);
 
-    const handleChange = (value: string[]) => {
-        console.log(`selected ${value}`);
-        setAnbieter(value);
-    };
-
-    const CheckboxGroupWissensbezug = Checkbox.Group;
-
-    const onChangeWissensbezug = (checkedValues: CheckboxValueType[]) => {
-        console.log('checked = ', checkedValues);
-        let array:string[]=[]
-        for (let index = 0; index < checkedValues.length; index++) {
-            array[index]=String(checkedValues[index]);
-        }
-        setWissensbezug(array);
-      };
-
-    const CheckboxGroupVerwaltungsspezifisch = Checkbox.Group;
-
-    const onChangeVerwaltungsspezifisch = (checkedValues: CheckboxValueType[]) => {
-        console.log('checked = ', checkedValues);
-        let value = false
-        if (String(checkedValues[0]) == "Einsteiger"){
-            value = true
-        }
-        setVerwaltungsspezifisch(value);
-    };
-
-    const CheckboxGroupZertifikat = Checkbox.Group;
-
-    const onChangeZertifikat = (checkedValues: CheckboxValueType[]) => {
-        console.log('checked = ', checkedValues);
-        let value = false
-        if (String(checkedValues[0]) == "Zertifikat"){
-            value = true
-        }
-        setZertifikat(value);
-    };
-
-    const CheckboxGroupKompetenzstufe = Checkbox.Group;
-
-    const onChangeKompetenzstufe = (checkedValues: CheckboxValueType[]) => {
-        console.log('checked = ', checkedValues);
-        let array:Skilllevel[]=[]
-        for (let index = 0; index < checkedValues.length; index++) {
-            let value = String(checkedValues[index]);
-            if (value == "Einsteiger"){
-                array[index]=0
-            }
-            else if (value == "Fortgeschritten"){
-                array[index]=1
-            }
-            else if (value == "Experte"){
-                array[index]=2
-            }
-        }
-        setKompetenzstufen(array);
-    };
-
-    const dauer: SliderMarks = {
-        0: '<10 Min.',
-        50: '1 Std.',
-        100: '8+Std.'
-      };
-
-    const tooltip: SliderTooltipProps = {
-        open: false
-        // Vielleicht formatter benutzen?
-      };
-
-    const onChangeDauer = (checkedValues: CheckboxValueType[]) => {
-        console.log('checked = ', checkedValues);
-        let array:string[]=[]
-        for (let index = 0; index < checkedValues.length; index++) {
-            let value = Number(checkedValues[index]);
-            if (value == 0){
-                array[index]="<10 Min."
-            }
-            else if (value == 50){
-                array[index]="1 Std."
-            }
-            else if (value == 100){
-                array[index]="8+Std."
-            }
-        }
-        setDauer(array);
-    };
-
-
-    const CheckboxGroupFormat = Checkbox.Group;
-
-    const onChangeFormat = (checkedValues: CheckboxValueType[]) => {
-        console.log('checked = ', checkedValues);
-        let array:Format[]=[]
-        for (let index = 0; index < checkedValues.length; index++) {
-            let value = String(checkedValues[index]);
-            if (value == "Live-Online"){
-                array[index]=0
-            }
-            else if (value == "Präsenz"){
-                array[index]=1
-            }
-            else if (value == "Hybrid"){
-                array[index]=2
-            }
-            else if (value == "Selbstorganisiert"){
-                array[index]=3
-            }
-        }
-        setFormat(array);
-    };
-
-    const onChangeStartdatum: DatePickerProps['onChange'] = (date, dateString) => {
-        console.log(date, dateString);
-        setStartdatum(date?.toDate)
-      };
-
-    const CheckboxGroupKosten = Checkbox.Group;
-
-    const onChangeKosten = (checkedValues: CheckboxValueType[]) => {
-        console.log('checked = ', checkedValues);
-        let value = false
-        if (String(checkedValues[0]) == "Kostenlos"){
-            value = true
-        }
-        setKosten(value);
-    };
-
-    useEffect(() => {
-        buildCourseFilterWsTo();
-    }, [Anbieter, Wissensbezug, Verwaltungsspezifisch, Zertifikat, Kompetenzstufen, Formate, Startdatum, Dauer, Kosten, Sonstiges])
-
     useEffect(() => {
         fetchWrapper.get('api/v1/courses/providers').then(res => {
-            let options:SelectProps['options']=[]
+            const options = []
             for (let i = 0; i < res.payload.length; i++) {
-                let value = String(res.payload[i]);
+                const value = String(res.payload[i]);
                 options.push({
                     label: value,
                     value,
@@ -191,77 +27,124 @@ const SearchOptions: React.FC<SearchOptionsProps> = ({onVariableChange}) => {
             }
             setOptions(options)
         });
-    }, options);
+    }, []);
+
+    const handleSelectChange = (value: string[]) => {
+        console.log(`selected ${value}`);
+        onFilterChange({anbieter: value});
+    };
+
+    const optionsWissensbezug = ["Theorie", "Praxis"];
+    const onChangeWissensbezug = (checkedValues: CheckboxValueType[]) => {
+        onFilterChange({wissensbezug: checkedValues});
+    };
+
+    const optionsVerwaltungsspezifisch = ['Verwaltungsspezifisch'];
+    const onChangeVerwaltungsspezifisch = (checkedValues: CheckboxValueType[]) => {
+        onFilterChange({verwaltungsspezifisch: checkedValues.includes("Verwaltungsspezifisch")});
+    };
+
+    const optionsZertifikat = ['Zertifikat'];
+    const onChangeZertifikat = (checkedValues: CheckboxValueType[]) => {
+        onFilterChange({zertifikat: checkedValues.includes("Zertifikat")});
+    };
+
+    const optionsKompetenzstufe = [{label: 'Einsteiger', value: '0'}, {
+        label: 'Fortgeschritten',
+        value: '1'
+    }, {label: 'Experte', value: '2'}];
+    const onChangeKompetenzstufe = (checkedValues: CheckboxValueType[]) => {
+        onFilterChange({kompetenzstufe: checkedValues});
+    };
+
+    const dauer: SliderMarks = {
+        0: '<10 Min.',
+        50: '1 Std.',
+        100: '8+Std.'
+    };
+
+    const tooltip: SliderTooltipProps = {
+        open: false
+        // Vielleicht formatter benutzen?
+    };
+
+    const onChangeDauer = (checkedValues: CheckboxValueType[]) => {
+        console.log('checked = ', checkedValues);
+        const array: string[] = []
+        for (let index = 0; index < checkedValues.length; index++) {
+            const value = Number(checkedValues[index]);
+            if (value == 0) {
+                array[index] = "<10 Min."
+            } else if (value == 50) {
+                array[index] = "1 Std."
+            } else if (value == 100) {
+                array[index] = "8+Std."
+            }
+        }
+        onFilterChange({dauer: array});
+    };
+
+    const optionsFormat = [
+        {label: 'Live-Online', value: '0'},
+        {label: 'Präsenz', value: '1'},
+        {label: 'Hybrid', value: '2'},
+        {label: 'Selbstorganisiert', value: '3'}
+    ];
+    const onChangeFormat = (checkedValues: CheckboxValueType[]) => {
+        onFilterChange({format: checkedValues});
+    };
+
+    const onChangeStartdatum: DatePickerProps['onChange'] = (_date, dateString) => {
+        onFilterChange({startdatum: dateString});
+    };
+
+    const optionsKosten = ['Kostenlos'];
+    const onChangeKosten = (checkedValues: CheckboxValueType[]) => {
+        onFilterChange({kosten: checkedValues.includes("Kostenlos")});
+    };
 
     return (
-        <Card title="Optionen" size="small" style={{minWidth:"250px", borderRadius:"20px", position:"sticky", top:"-500px"}}>
+        <Card className="searchfilter" title="Optionen" size="small"
+              style={{minWidth: "250px", borderRadius: "20px", position: "sticky", top: "-500px"}}>
+
             <p>Anbieter</p>
             <Select
                 mode="multiple"
-                style={{ width: '100%' }}
+                style={{width: '100%'}}
                 placeholder="Anbieter auswählen..."
                 defaultValue={[]}
-                onChange={handleChange}
+                onChange={handleSelectChange}
                 options={options}
                 maxTagCount={'responsive'}
             />
             <Divider/>
             <p>Wissensbezug</p>
-                <CheckboxGroupWissensbezug onChange={onChangeWissensbezug}>
-                    <Space direction="vertical">
-                        <Checkbox value="Theorie">Theorie</Checkbox>
-                        <Checkbox value="Praxis">Praxis</Checkbox>
-                    </Space>
-                </CheckboxGroupWissensbezug>
+            <Checkbox.Group onChange={onChangeWissensbezug} options={optionsWissensbezug}/>
             <Divider/>
             <p>Verwaltungsspezifisch</p>
-                <CheckboxGroupVerwaltungsspezifisch onChange={onChangeVerwaltungsspezifisch}>
-                    <Space direction="vertical">
-                        <Checkbox value="Verwaltungsspezifisch">Verwaltungsspezifisch</Checkbox>
-                    </Space>
-                </CheckboxGroupVerwaltungsspezifisch>
+            <Checkbox.Group onChange={onChangeVerwaltungsspezifisch} options={optionsVerwaltungsspezifisch}/>
             <Divider/>
             <p>Zertifikat</p>
-                <CheckboxGroupZertifikat onChange={onChangeZertifikat}>
-                    <Space direction="vertical">
-                        <Checkbox value="Zertifikat">Zertifikat</Checkbox>
-                    </Space>
-                </CheckboxGroupZertifikat>
+            <Checkbox.Group onChange={onChangeZertifikat} options={optionsZertifikat}/>
             <Divider/>
             <p>Kompetenzstufe</p>
-                <CheckboxGroupKompetenzstufe onChange={onChangeKompetenzstufe}>
-                    <Space direction="vertical">
-                        <Checkbox value="Einsteiger">Einsteiger</Checkbox>
-                        <Checkbox value="Fortgeschritten">Fortgeschritten</Checkbox>
-                        <Checkbox value="Experte">Experte</Checkbox>
-                    </Space>
-                </CheckboxGroupKompetenzstufe>
+            <Checkbox.Group onChange={onChangeKompetenzstufe} options={optionsKompetenzstufe}/>
             <Divider/>
             <p>Dauer</p>
-            <Slider range marks={dauer} onChange={onChangeDauer} tooltip={tooltip} step={null} defaultValue={[0,100]} style={{marginLeft:"20px", marginRight:"20px"}}/>
+            <Slider range marks={dauer} onChange={onChangeDauer} tooltip={tooltip} step={null} defaultValue={[0, 100]}
+                    style={{marginLeft: "20px", marginRight: "20px"}}/>
             <Divider/>
             <p>Format</p>
-                <CheckboxGroupFormat onChange={onChangeFormat}>
-                    <Space direction="vertical">
-                        <Checkbox value="Live-Online">Live-Online</Checkbox>
-                        <Checkbox value="Präsenz">Präsenz</Checkbox>
-                        <Checkbox value="Hybrid">Hybrid</Checkbox>
-                        <Checkbox value="Selbstorganisiert">Selbstorganisiert</Checkbox>
-                    </Space>
-                </CheckboxGroupFormat>
+            <Checkbox.Group onChange={onChangeFormat} options={optionsFormat}/>
             <Divider/>
             <p>Startdatum</p>
-                <DatePicker onChange={onChangeStartdatum} placeholder="Datum"/>
+            <DatePicker onChange={onChangeStartdatum} placeholder="Datum"/>
             <Divider/>
             <p>Kosten</p>
-                <CheckboxGroupKosten onChange={onChangeKosten}>
-                    <Space direction="vertical">
-                        <Checkbox value="Kostenlos">Kostenlos</Checkbox>
-                    </Space>
-                </CheckboxGroupKosten>
+            <Checkbox.Group onChange={onChangeKosten} options={optionsKosten}/>
             <Divider/>
             <p>Sonstiges</p>
-            </Card>
+        </Card>
     );
 }
 
--- src/pages/Searching.tsx
+++ src/pages/Searching.tsx
@@ -2,107 +2,83 @@ import {useEffect, useState} from "react";
 import SearchComponent from "../components/SearchComponent.tsx";
 import {Course, CourseFilterWsTo} from "../interfaces.ts";
 import {fetchWrapper} from "../api/helper.ts";
-import {useLocation, useNavigate} from "react-router-dom";
+import {useLocation} from "react-router-dom";
 import {Button} from 'antd';
 import SearchOptions from "../components/SearchOptions.tsx";
+import Search from "antd/es/input/Search";
 
 function Searching() {
 
-    const loc = useLocation();
+    const limit = 10;
 
-    const [tagIDs, setTagIDs] = useState<number[]>([]);
+    const location = useLocation();
 
+    const [courses, setCourses] = useState<Course[]>([]);
+    const [offset, setOffset] = useState(0);
+    const [searchStr, setSearchStr] = useState<string>(location.state?.searchStr ?? "");
     const [courseFilter, setCourseFilter] = useState<CourseFilterWsTo>({
-        tagIDs: loc.state?.tagIDs,
-        Anbieter: [],
-        Wissensbezug: [],
-        Verwaltungsspezifisch: false,
-        Zertifikat: false,
-        Kompetenzstufe: [],
-        Format: [],
-        Startdatum: undefined,
-        Dauer: [],
-        Kosten: false,
-        Sonstiges: []
+        tagIDs: location.state?.tagIDs ?? [],
+        anbieter: [],
+        wissensbezug: [],
+        verwaltungsspezifisch: false,
+        zertifikat: false,
+        kompetenzstufe: [],
+        format: [],
+        startdatum: undefined,
+        dauer: [],
+        kosten: false,
+        sonstiges: []
     });
 
+    const onSearch = (value: string) => {
+        setSearchStr(value);
+    }
+
     // Define the callback function
-    const handleVariableChange = (variable: CourseFilterWsTo):void => {
-        setCourseFilter(variable);
-        console.log(courseFilter)
+    const onFilterChange = (filter: object): void => {
+        setCourseFilter(courseFilter => ({...courseFilter, ...filter}));
     };
 
-    const [courseFeedbacks, setCourseFeedbacks] = useState<{ course: Course, feedback: number }[]>([]);
-    const [limit, setLimit] = useState(10);
-    const [offset, setOffset] = useState(0);
-
-    const searchStr = location.pathname.split('/').pop();
-
     const handlePagination = () => {
-        setLimit(limit+10)
+        setOffset(offset => offset + limit)
     }
-
-    const handleTagIDs = (tagIDs: number[]) =>{
-        let courseFilterWsTo: CourseFilterWsTo = {
-            tagIDs: tagIDs,
-            Anbieter: courseFilter.Anbieter,
-            Wissensbezug: courseFilter.Wissensbezug,
-            Verwaltungsspezifisch: courseFilter.Verwaltungsspezifisch,
-            Zertifikat: courseFilter.Zertifikat,
-            Kompetenzstufe: courseFilter.Kompetenzstufe,
-            Format: courseFilter.Format,
-            Startdatum: courseFilter.Startdatum,
-            Dauer: courseFilter.Dauer,
-            Kosten: courseFilter.Kosten,
-            Sonstiges: []
-        }
-        setCourseFilter(courseFilterWsTo)
-        return courseFilterWsTo
-    }
-
-    const navigate = useNavigate();
 
     useEffect(() => {
-        handleTagIDs(loc.state?.tagIDs);
-        fetchWrapper.post('api/v1/filter/limit/'+ limit + '/offset/' + offset + '/' + searchStr, handleTagIDs(loc.state?.tagIDs)).then((res) => {
-            const coursePromises = res.payload.map((course: Course) => {
-                return fetchWrapper.get('api/v1/feedback/average/course/' + course.id)
-                    .then((res) => ({course, feedback: res.payload}))
-                    .catch((error) => {
-                        console.error(error);
-                        return {course, feedback: 0.0};
-                    });
-            });
-
-            Promise.all(coursePromises).then(setCourseFeedbacks);
+        fetchWrapper.post('api/v1/filter/limit/' + limit + '/offset/' + offset + '/' + searchStr, courseFilter).then((res) => {
+            if (offset === 0)
+                setCourses(res.payload);
+            else
+                setCourses([...courses, ...res.payload]);
         });
-    }, [limit, offset, searchStr, navigate, loc.state]);
+    }, [limit, offset, searchStr, courseFilter]);
 
     return (
         <div style={{display: "flex", width: "100%", justifyContent: "center", flexDirection: "row", flexWrap: "wrap"}}>
-            <div style={{flexBasis: "100%", textAlign: "center", marginTop: "1rem"}}>
-                {
-                    searchStr ?
-                        <h1 style={{margin: "0 0 5px 0"}}>{searchStr}</h1>
-                        : <h1 style={{margin: "0 0 5px 0"}}>Alle unsere Kursangebote!</h1>
-                }
+            <div style={{flexBasis: "100%", textAlign: "center", marginTop: "1rem", maxWidth: "400px"}}>
+                <Search size="large" defaultValue={searchStr} placeholder="Kursangebote durchsuchen"
+                        onSearch={onSearch}/>
             </div>
-            <div style={{display: "flex", width: "100%", justifyContent: "center", flexDirection: "row", flexWrap: "wrap"}}>
-                <div style={{margin:"1rem"}}>
-                    <SearchOptions onVariableChange={handleVariableChange}/>
+            <div style={{
+                display: "flex",
+                width: "100%",
+                justifyContent: "center",
+                flexDirection: "row",
+                flexWrap: "wrap"
+            }}>
+                <div style={{margin: "1rem"}}>
+                    <SearchOptions courseFilter={courseFilter} onFilterChange={onFilterChange}/>
                 </div>
-                <div style={{flex:"1", maxWidth:"1000px", marginRight:"1rem"}}>
-                {
-                    courseFeedbacks ?
-                        courseFeedbacks.map((item: any) => <div key={item.course.id}><SearchComponent obj={item.course}
-                                                                                                      feedbackrate={item.feedback}/>
-                        </div>)
-                        : <SearchComponent/>
-                }
-            </div>
-            <div style={{flexBasis: "100%", display:"flex", justifyContent:"center", marginTop:"20px"}}>
-                <Button type="primary" onClick={handlePagination}>Mehr anzeigen</Button>
-            </div>
+                <div style={{flex: "1", maxWidth: "1000px", marginRight: "1rem"}}>
+                    {
+                        courses ?
+                            courses.map((course: Course) => <div key={course.id}><SearchComponent obj={course}/>
+                            </div>)
+                            : <SearchComponent/>
+                    }
+                </div>
+                <div style={{flexBasis: "100%", display: "flex", justifyContent: "center", marginTop: "20px"}}>
+                    <Button type="primary" onClick={handlePagination}>Mehr anzeigen</Button>
+                </div>
             </div>
         </div>
 
