46fc11f6e84895eb52a182ca9d01922cef472b33
Fabian <1803fabian@gmail.com>
2023-12-16 21:58:18 +0100

    switched to custom carousel implementation

--- src/components/RecomSlider.tsx
+++ src/components/RecomSlider.tsx
@@ -1,35 +1,116 @@
-import { Skeleton } from "antd";
+import {Button, Skeleton} from "antd";
 import Recommendation from "./Recommendation.tsx";
+import './RecomSlider.css'
 
-import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 import {Course} from "../interfaces.ts";
-
-const responsive = {
-    desktop: {
-        breakpoint: {max: 3000, min: 1200},
-        items: 5,
-        slidesToSlide: 2 // optional, default to 1.
-    },
-    tablet: {
-        breakpoint: {max: 1200, min: 950},
-        items: 4,
-        slidesToSlide: 2 // optional, default to 1.
-    },
-    mobile: {
-        breakpoint: {max: 950, min: 700},
-        items: 3,
-        slidesToSlide: 1
-    },
-    small: {
-        breakpoint: {max: 700, min: 0},
-        items: 1,
-        slidesToSlide: 1
-    }
-
-};
+import {LeftOutlined, RightOutlined} from "@ant-design/icons";
+import React, {useCallback, useLayoutEffect, useRef, useState} from "react";
 
 function RecomSlider({title, data}: { title?: string, data?: Course[] }) {
+
+    const [translate, setTranslate] = useState(0);
+    const [dragging, setDragging] = useState(false);
+    const [transition, setTransition] = useState(false);
+
+    const ref = useRef<any>(null);
+    const contentWidth = 240;
+
+    const [max, setMax] = useState(740);
+
+    useLayoutEffect(() => {
+        function updateSize() {
+            console.log(ref.current.offsetWidth)
+            if (ref.current && ref.current.offsetWidth) {
+                setMax((data ? (data.length) * contentWidth : 0) - (ref.current.offsetWidth - 20))
+                console.log((data ? (data.length) * contentWidth : 0) - (ref.current.offsetWidth - 20))
+            }
+        }
+
+        window.addEventListener('resize', updateSize);
+        updateSize();
+        return () => window.removeEventListener('resize', updateSize);
+    }, [data, ref]);
+
+
+    const listener = useCallback((e: MouseEvent) => {
+        setTranslate(translate => {
+            const offset = translate + e.movementX;
+
+            console.log(offset)
+
+            if (offset < -max - 120)
+                return -max - 120;
+
+            return offset > 120 ? 120 : offset
+        })
+        if (e.movementX !== 0)
+            setDragging(true);
+    }, [max])
+
+    const onmousedown = (e: React.MouseEvent<HTMLElement>) => {
+        if(max > 0){
+            console.log("mousedown")
+            window.addEventListener("mousemove", listener)
+            e.preventDefault();
+            setDragging(false)
+        }
+    }
+
+    const onmouseup = (e: React.MouseEvent<HTMLElement>) => {
+        console.log("mouseup")
+        window.removeEventListener("mousemove", listener)
+        e.preventDefault();
+        snap();
+    }
+
+    const onmouseleave = () => {
+        console.log("mouseleave")
+        window.removeEventListener("mousemove", listener)
+        setDragging(false)
+        snap();
+    }
+
+    const onclick = (e: React.MouseEvent<HTMLElement>) => {
+        console.log("click")
+        if (dragging)
+            e.stopPropagation();
+        setDragging(false);
+    }
+
+    const snap = () => {
+        setTransition(true);
+
+        setTranslate(translate => {
+            const offset = Math.abs(translate % 240);
+
+            if (Math.abs(translate) >= Math.abs(max))
+                return -max;
+            if (translate > 0)
+                return 0;
+            if (offset < 120)
+                return translate + offset;
+            else
+                return translate - (240 - offset);
+        })
+
+        setTimeout(() => {
+            setTransition(false);
+        }, 200);
+    }
+
+    const navOnClick = (direction: "l" | "r") => {
+        setTransition(true);
+        if (direction == "l" && translate < 0)
+            setTranslate(translate => translate + 240);
+        else if (direction == "r" && Math.abs(translate) < max) {
+            if (Math.abs(translate) + 240 > max)
+                setTranslate(-max);
+            else
+                setTranslate(translate => translate - 240);
+        }
+    }
+
     return (
         <>
             <div id="slider" style={{
@@ -39,6 +120,7 @@ function RecomSlider({title, data}: { title?: string, data?: Course[] }) {
                 margin: "auto",
                 display: "flex",
                 flexDirection: "column",
+                position: "relative",
             }}>
                 {
                     title ?
@@ -46,17 +128,35 @@ function RecomSlider({title, data}: { title?: string, data?: Course[] }) {
                         : <Skeleton.Input active style={{margin: "0 0 5px 0"}}/>
                 }
 
-                <div style={{background: "#D9D9D9", borderRadius: "20px", padding: "10px"}}>
-                    <Carousel responsive={responsive}>
-                        {
-                            data ?
-                            data.map((item: Course) => <div key={item.id}><Recommendation obj={item}/></div>)
+                <div onMouseDown={onmousedown} onMouseUp={onmouseup} onClickCapture={onclick}
+                     onMouseLeave={onmouseleave} className="slider" ref={ref}
+                     style={{
+                         background: "#D9D9D9",
+                         display: "flex",
+                         flexDirection: "row",
+                         overflowX: "hidden",
+                         padding: "10px 10px",
+                         borderRadius: "20px",
+                         position: "relative",
+                     }}>
+                    {
+                        data ?
+                            data.map((item: Course) => <div key={item.id}><Recommendation
+                                style={{
+                                    transform: `translate(${translate}px)`,
+                                    transition: `${transition ? "all 0.2s ease-in-out" : ""}`
+                                }} obj={item}/></div>)
                             : <Recommendation/>
-                        }
-                    </Carousel>
+                    }
                 </div>
-
+                <Button className="showmore right" type="text" icon={<RightOutlined/>}
+                        style={{display: `${Math.abs(translate) < max ? "block" : "none"}`}}
+                        onClick={() => navOnClick("r")}/>
+                <Button className="showmore left" type="text" icon={<LeftOutlined/>}
+                        style={{display: `${translate < 0 ? "block" : "none"}`}}
+                        onClick={() => {navOnClick("l"); snap()}}/>
             </div>
+
         </>
     );
 }
--- src/components/Recommendation.tsx
+++ src/components/Recommendation.tsx
@@ -3,8 +3,9 @@ import './Recommendation.css'
 import {Course} from "../interfaces.ts";
 import Bookmark from "./Bookmark.tsx";
 import {useNavigate} from "react-router-dom";
+import {CSSProperties} from "react";
 
-function Recommendation({obj}: { obj?: Course }) {
+function Recommendation({obj, style}: { obj?: Course, style?: CSSProperties }) {
 
     const navigate = useNavigate();
 
@@ -15,44 +16,45 @@ function Recommendation({obj}: { obj?: Course }) {
             navigate(`/detail/${obj.id}`, {state: {obj: obj}});
     }
 
-    return (<a>
-        <div id="recom" onClick={handleClick}>
-            <div id="recompic">
+    return (
+        <a title={obj?.name ?? ""}>
+            <div id="recom" onClick={handleClick} style={{...style, display: "flex"}}>
+                <div id="recompic">
+                    {
+                        obj ?
+                            <img style={{
+                                objectFit: "cover",
+                                borderRadius: "10px",
+                                width: "220px",
+                                height: "100%",
+                                userSelect: "none",
+                                pointerEvents: "none"
+                            }} src={obj.image}/>
+                            : <Skeleton.Image className="" style={{
+                                objectFit: "cover",
+                                borderRadius: "10px",
+                                width: "220px",
+                                height: "100%",
+                                userSelect: "none",
+                                pointerEvents: "none"
+                            }} active/>
+                    }
+                    {obj && obj.id ?
+                        <Bookmark id={obj.id} style={{position: "absolute", top: "10px", right: "10px"}}/>
+                        : <></>}
+                </div>
                 {
                     obj ?
-                        <img style={{
-                            objectFit: "cover",
-                            borderRadius: "10px",
-                            width: "220px",
-                            height: "100%",
-                            userSelect: "none",
-                            pointerEvents: "none"
-                        }} src={obj.image}/>
-                        : <Skeleton.Image className="" style={{
-                            objectFit: "cover",
-                            borderRadius: "10px",
-                            width: "220px",
-                            height: "100%",
-                            userSelect: "none",
-                            pointerEvents: "none"
-                        }} active/>
+                        <div>
+                            <Typography.Title className="coursename" level={5} ellipsis={{rows: 2}}
+                                              style={{margin: "5px", fontWeight: "bold"}}>
+                                {obj.name}
+                            </Typography.Title>
+                        </div>
+                        : <Skeleton.Input active size="small" style={{margin: "5px"}}/>
                 }
-                {obj && obj.id ?
-                    <Bookmark id={obj.id} style={{position: "absolute", top: "10px", right: "10px"}}/>
-                    : <></>}
             </div>
-            {
-                obj ?
-                    <a className="courselink" title={obj.name}>
-                        <Typography.Title className="coursename" level={5} ellipsis={{rows: 2}}
-                                          style={{margin: "5px", fontWeight: "bold"}}>
-                            {obj.name}
-                        </Typography.Title>
-                    </a>
-                    : <Skeleton.Input active size="small" style={{margin: "5px"}}/>
-            }
-        </div>
-    </a>);
+        </a>);
 }
 
 export default Recommendation;
