21b3c0d788a66e6fde3368cc7c4b4a3258ea8634
ralbers <ralbers@uni-muenster.de>
2024-01-01 21:04:21 +0100

    Design improvements

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,6 +1,6 @@
 import {ChangeEventHandler, useEffect, useState} from "react";
 import {UserOutlined} from "@ant-design/icons";
-import {Skeleton, Button, Modal, Switch, Input, Form} from "antd";
+import {Skeleton, Button, Modal, Switch, Input, Form, Divider} from "antd";
 import './Profile.css';
 import edit from '../assets/edit.png';
 import { EMAIL_WRONG_FORMAT, ENTER_EMAIL } from "../constants/de.ts";
@@ -198,73 +198,123 @@ function Profile() {
     };
 
     return (
-        <div style={{display: "flex", flexWrap:"wrap",justifyContent: "center", marginLeft:"25%", marginRight:"25%"}}>
-            <div>
-                <h1 >Profil</h1>
-            </div>
-            <hr style={{width:"100%" }}/>
-            <div style={{flexDirection:"row", flexWrap:"wrap", display:"flex", flexBasis: "100%"}}>
-                {email ? <p style={{flex:"80%", width:"100%"}}>Ihre E-Mailadresse: {email}</p> : <Skeleton active />}
-                <div  style={{flex:"20%", display:"flex", flexDirection:"row-reverse", alignItems:"center"}}>
-                    <Button type="primary" onClick={showModalMail}><img src={edit} style={{width:"15px"}}></img></Button>
-                </div>
-                <Modal title="E-Mail ändern" open={isModalMailOpen} onOk={handleOkMail} onCancel={handleCancelMail}>
-                    <p>Hier können Sie Ihre E-Mailadresse ändern:</p>
-                    <Form>
-                        <Form.Item
-                            name="email"
-                            rules={[
-                                {required: true, message: ENTER_EMAIL},
-                                {type: 'email', message: EMAIL_WRONG_FORMAT}
-                            ]}
-                        >
-                            <Input prefix={<UserOutlined className="site-form-item-icon"/>} placeholder="E-Mail" onChange={onChangeEmail} style={{marginBottom:"5px"}}/>
-                        </Form.Item>
-                        <Input.Password prefix={<Key/>} placeholder="Passwort" onChange={onChangePassword} hidden={true}/>
-                    </Form>
-                </Modal>
-            </div>
-            <div style={{flexDirection:"row", flexWrap:"wrap", display:"flex", flexBasis: "100%"}}>
-                {name ? <p style={{flex:"85%"}}>Ihr Name: {name}</p> : <Skeleton active />}
-                <div  style={{flex:"15%", display:"flex", flexDirection:"row-reverse", alignItems:"center"}}>
-                    <Button type="primary" onClick={showModalName}><img src={edit} style={{width:"15px"}}></img></Button>
-                </div>
-                <Modal title="Name ändern" open={isModalNameOpen} onOk={handleOkName} onCancel={handleCancelName}>
-                    <p>Hier können Sie Ihren Namen ändern:</p>
-                    <Input prefix={<UserOutlined className="site-form-item-icon"/>} placeholder="Name" onChange={onChangeName} style={{marginBottom:"5px"}}/>
-                    <Input.Password prefix={<Key/>} placeholder="Passwort" onChange={onChangePassword} hidden={true}/>
-                </Modal>
-            </div>
-            <div style={{flexDirection:"row", flexWrap:"wrap", display:"flex", flexBasis: "100%"}}>
-                {name ? <p style={{flex:"85%"}}>Passwort ändern</p> : <Skeleton active />}
-                <div  style={{flex:"15%", display:"flex", flexDirection:"row-reverse", alignItems:"center"}}>
-                    <Button type="primary" onClick={showModalOldPassword}><img src={edit} style={{width:"15px"}}></img></Button>
-                </div>
-                <Modal title="Passwort ändern" open={isModalOldPasswordOpen} onOk={handleOkOldPassword} onCancel={handleCancelOldPassword}>
-                    <p>Geben Sie hier bitte Ihr altes Passwort ein.</p>
-                    <Input.Password prefix={<Key/>} placeholder="Passwort" onChange={onChangeOldPassword} hidden={true}/>
-                </Modal>
-                <Modal title="Passwort ändern" open={isModalNewPasswordOpen} onOk={handleOkNewPassword} onCancel={handleCancelNewPassword}>
-                    <p>Geben Sie hier bitte Ihr neues Passwort ein.</p>
-                    <Input.Password prefix={<Key/>} placeholder="Passwort" onChange={onChangeNewPassword} hidden={true}/>
-                </Modal>
-            </div>
-            <hr style={{width:"100%" }}/>
-            <div style={{flexBasis: "100%"}}>
+        <div style={{width:"100%", display:"flex", justifyContent:"center"}}>
+            <div style={{display: "flex", flexWrap:"wrap",justifyContent: "center", minWidth:"400px",maxWidth:"800px", marginLeft:"20%", marginRight:"20%", background:"#F9F9F9", borderRadius:"20px", marginTop:"25px", paddingLeft:"15px", paddingRight:"15px", color:"#3F3F3F"}}>
                 <div>
-                    <h2 style={{textAlign:"center"}}>Benachrichtigung</h2>
+                    <h1 >Profil</h1>
                 </div>
-                <div style={{flexDirection:"row", flexWrap:"wrap", display:"flex", flexBasis: "100%"}}>
-                    <p style={{flex:"85%"}}>Benachrichtigung zu neuen Kursempfehlungen</p>
-                    <div style={{flex:"15%", display:"flex", flexDirection:"row-reverse", alignItems:"center"}}>
-                        <Switch defaultChecked onChange={onChange} />
+                <div style={{background:"#F7F7F7", borderRadius:"10px", paddingLeft:"15px", paddingRight:"15px", flexBasis: "100%"}}>
+                    <div style={{flexDirection:"row", flexWrap:"wrap", display:"flex", flexBasis: "100%"}}>
+                        {email ? <p style={{flex:"80%", width:"100%"}}>Ihre E-Mailadresse: {email}</p> : <Skeleton active />}
+                        <div  style={{flex:"20%", display:"flex", flexDirection:"row-reverse", alignItems:"center"}}>
+                            <Button type="primary" onClick={showModalMail}><img src={edit} style={{width:"15px"}}></img></Button>
+                        </div>
+                        <Modal title="E-Mail ändern" open={isModalMailOpen} onOk={handleOkMail} onCancel={handleCancelMail}
+                        footer={[
+                            <Button key="back" onClick={handleCancelMail}>
+                            Abbrechen
+                            </Button>,
+                            <Button
+                            type="primary"
+                            onClick={handleOkMail}
+                            >
+                            E-Mail ändern
+                            </Button>,
+                        ]}
+                        >
+                            <p>Hier können Sie Ihre E-Mailadresse ändern:</p>
+                            <Form>
+                                <Form.Item
+                                    name="email"
+                                    rules={[
+                                        {required: true, message: ENTER_EMAIL},
+                                        {type: 'email', message: EMAIL_WRONG_FORMAT}
+                                    ]}
+                                >
+                                    <Input prefix={<UserOutlined className="site-form-item-icon"/>} placeholder="E-Mail" onChange={onChangeEmail} style={{marginBottom:"5px"}}/>
+                                </Form.Item>
+                                <Input.Password prefix={<Key/>} placeholder="Passwort" onChange={onChangePassword} hidden={true}/>
+                            </Form>
+                        </Modal>
+                    </div>
+                    <div style={{flexDirection:"row", flexWrap:"wrap", display:"flex", flexBasis: "100%"}}>
+                        {name ? <p style={{flex:"85%"}}>Ihr Name: {name}</p> : <Skeleton active />}
+                        <div  style={{flex:"15%", display:"flex", flexDirection:"row-reverse", alignItems:"center"}}>
+                            <Button type="primary" onClick={showModalName}><img src={edit} style={{width:"15px"}}></img></Button>
+                        </div>
+                        <Modal title="Name ändern" open={isModalNameOpen} onOk={handleOkName} onCancel={handleCancelName}
+                        footer={[
+                            <Button key="back" onClick={handleCancelName}>
+                                Abbrechen
+                            </Button>,
+                            <Button
+                                type="primary"
+                                onClick={handleOkName}
+                            >
+                                Name ändern
+                            </Button>,
+                            ]}
+                            >
+                            <p>Hier können Sie Ihren Namen ändern:</p>
+                            <Input prefix={<UserOutlined className="site-form-item-icon"/>} placeholder="Name" onChange={onChangeName} style={{marginBottom:"5px"}}/>
+                            <Input.Password prefix={<Key/>} placeholder="Passwort" onChange={onChangePassword} hidden={true}/>
+                        </Modal>
+                    </div>
+                    <div style={{flexDirection:"row", flexWrap:"wrap", display:"flex", flexBasis: "100%"}}>
+                        {name ? <p style={{flex:"85%"}}>Passwort ändern</p> : <Skeleton active />}
+                        <div  style={{flex:"15%", display:"flex", flexDirection:"row-reverse", alignItems:"center"}}>
+                            <Button type="primary" onClick={showModalOldPassword}><img src={edit} style={{width:"15px"}}></img></Button>
+                        </div>
+                        <Modal title="Passwort ändern" open={isModalOldPasswordOpen} onOk={handleOkOldPassword} onCancel={handleCancelOldPassword}                 
+                        footer={[
+                            <Button key="back" onClick={handleCancelOldPassword}>
+                            Abbrechen
+                            </Button>,
+                            <Button
+                            type="primary"
+                            onClick={handleOkOldPassword}
+                            >
+                            Weiter
+                            </Button>,
+                        ]}>
+                            <p>Geben Sie hier bitte Ihr altes Passwort ein.</p>
+                            <Input.Password prefix={<Key/>} placeholder="Passwort" onChange={onChangeOldPassword} hidden={true}/>
+                        </Modal>
+                        <Modal title="Passwort ändern" open={isModalNewPasswordOpen} onOk={handleOkNewPassword} onCancel={handleCancelNewPassword}
+                            footer={[
+                                <Button key="back" onClick={handleCancelNewPassword}>
+                                    Abbrechen
+                                </Button>,
+                                <Button
+                                    type="primary"
+                                    onClick={handleOkNewPassword}
+                                >
+                                    Passwort ändern
+                                </Button>,
+                                ]}
+                        >
+                            <p>Geben Sie hier bitte Ihr neues Passwort ein.</p>
+                            <Input.Password prefix={<Key/>} placeholder="Passwort" onChange={onChangeNewPassword} hidden={true}/>
+                        </Modal>
                     </div>
                 </div>
-            </div>
-            <hr style={{width:"100%" }}/>
-            <div style={{flexBasis: "100%"}}>
-                <h2 style={{textAlign:"center"}}>Datenschutz</h2>
-                <p style={{textAlign:"left"}}>Hier werden irgendwann einmal Informationen zum Datenschutz stehen: Cookies, Datenschutzerklärung etc.</p>
+                <Divider/>
+                <div style={{flexBasis: "100%"}}>
+                    <div>
+                        <h2 style={{textAlign:"center"}}>Benachrichtigung</h2>
+                    </div>
+                    <div style={{flexDirection:"row", flexWrap:"wrap", display:"flex", flexBasis: "100%", background:"#F7F7F7", borderRadius:"10px", paddingLeft:"15px", paddingRight:"15px"}}>
+                        <p style={{flex:"85%"}}>Benachrichtigung zu neuen Kursempfehlungen</p>
+                        <div style={{flex:"15%", display:"flex", flexDirection:"row-reverse", alignItems:"center"}}>
+                            <Switch defaultChecked onChange={onChange} />
+                        </div>
+                    </div>
+                </div>
+                <Divider/>
+                <div style={{flexBasis: "100%", marginBottom:"0px"}}>
+                    <h2 style={{textAlign:"center"}}>Datenschutz</h2>
+                    <p style={{textAlign:"left", background:"#F7F7F7", borderRadius:"10px", padding:"15px"}}>Hier werden irgendwann einmal Informationen zum Datenschutz stehen: Cookies, Datenschutzerklärung etc.</p>
+                </div>
             </div>
         </div>
     );
