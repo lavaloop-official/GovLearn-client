7222c86d418d6f30500df595c84ee60540d15f00
ralbers <ralbers@uni-muenster.de>
2024-01-07 21:25:27 +0100

    Adjust design of Invitations

--- src/components/Group/GroupInvitation.tsx
+++ src/components/Group/GroupInvitation.tsx
@@ -16,16 +16,16 @@ function GroupInvitation({group, acceptInvitation, denyInvitation}:{group:GroupI
     }
 
     return (
-        <Flex vertical style={{background:"grey", borderRadius:"10px", padding:"4px"}}>
+        <Flex vertical style={{background:"#D9D9D9", borderRadius:"10px", padding:"0px"}}>
         <div onClick={() => {setExpanded(!expanded)}} style={{display:"flex", flexDirection:"row", justifyContent:"space-between", alignItems:"center", cursor:"pointer"}}>
-            <p style={{color:"white", fontSize:"18px", margin:"10px", overflow:"auto", whiteSpace:"nowrap"}} className="scrollbar">{group.groupName}</p>
+            <p style={{color:"#3F3F3F", fontSize:"18px", margin:"10px", overflow:"auto", whiteSpace:"nowrap"}} className="scrollbar">{group.groupName}</p>
             <div style={{marginRight:"5px", display:"flex", gap:"5px", justifyContent:"right"}}>
                 <Button onClick={onDenyInvitation} icon={<XLg color="red"/>}></Button>
                 <Button onClick={onAcceptInvitation} icon={<CheckLg color="green"/>}></Button>
             </div>
         </div>
             {
-                expanded ? <div style={{background:"lightgray", padding:"5px", borderRadius: "10px"}}>{group.groupDescription}</div> : <div/>
+                expanded ? <div style={{background:"#F4F4F4", color:"#3F3F3F", padding:"5px", margin:"5px", borderRadius: "5px"}}>{group.groupDescription}</div> : <div/>
             }
         </Flex>
     )
